aaa4c0ddf6dfb1148240b3c35690092c2c4aa3f1
Charly Sparks <charlysparks@users.noreply.github.com>
2017-07-15 02:50:19 -0400

    Create secret_message.py

---
+++ secret_message.py
@@ -0,0 +1,10 @@
+import re
+​
+def find_message(text):
+    return ''.join(re.findall('[A-Z]', text))
+​
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_message("How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message("hello world!") == "", "Nothing"
+    assert find_message("HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
